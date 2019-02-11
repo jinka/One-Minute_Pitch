6c845ef2d618fc13c89c652cfe3e9b4761ecfbe1
jinka <voiphandy@gmail.com>
2019-02-11 16:02:22 +0300

    initialized migrate class

--- manage.py
+++ manage.py
@@ -7,10 +7,14 @@ from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
 
+
 manager = Manager(app)
 manager.add_command('server',Server)
 
-manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+# manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
