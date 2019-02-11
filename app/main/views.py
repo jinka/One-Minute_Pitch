269e3cb9e18891dc4d5f14802c8ec11afdd49b65
jinka <voiphandy@gmail.com>
2019-02-11 19:19:53 +0300

    Add Flask_login to impelement methods like is_authenticated(), is_active(), is_anonymous() and get_id()

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,20 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort
 from . import main
+from flask_login import login_required
 
-# Views
-@app.route('/')
+# Views 
+@main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
 
+    title = "Welcome to One Time Pitch"
+    return render_template('index.html', title = title)
+
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
 
