d6507db3849b24a8a06ccd3814adde2dfd62a5d5
jinka <voiphandy@gmail.com>
2019-02-11 15:34:35 +0300

    Class Role created to define all different roles

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
- SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/watchlist'
+ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daud:jinkoos@localhost/watchlist'
 
 class ProdConfig(Config):
     pass
