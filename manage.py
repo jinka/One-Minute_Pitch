df683c21190132c2ecc00ae546aa1ea1b89cda7d
jinka <voiphandy@gmail.com>
2019-02-11 16:00:30 +0300

    Import flask migrate into manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app
 from flask_script import Manager,Server
 from app import create_app,db
 from app.models import User,Role
-
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
