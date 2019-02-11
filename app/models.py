750e68ee9fdce43de9e3b192f2e09b3d58f28716
jinka <voiphandy@gmail.com>
2019-02-11 19:40:57 +0300

    Created new user registration form

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-
+from . import login_manager
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -11,8 +11,10 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(255))
-
-
+ 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     @property
     def password(self):
