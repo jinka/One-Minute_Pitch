aa331707ea7a102f049247502e08951225574973
jinka <voiphandy@gmail.com>
2019-02-12 13:55:09 +0300

    Installed and imported uploadset class to define the type of file to upload

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,11 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
+photos = UploadSet('photos',IMAGES)
+def create_app(config_name):
+    
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
